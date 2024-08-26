e5518f55d7c2fa92b20d0ecac39216c196cfc0fc
sgc85 <79593413+sgc85@users.noreply.github.com>
2024-08-26 13:45:10 +0000

    show spinner on loading genres

--- gamereviews/src/components/GenreList.tsx
+++ gamereviews/src/components/GenreList.tsx
@@ -1,18 +1,26 @@
 // import useData from "../hooks/useData";
 // import { Genre } from "../hooks/useGenres";
-import { HStack, List, ListItem, Image, Text } from "@chakra-ui/react";
+import { HStack, List, ListItem, Image, Text, Spinner } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedUrl from "../services/image-url";
 
 const GenreList = () => {
-  const { data, isLoading } = useGenres();
+  const { data, loading, error } = useGenres();
+
+  if (loading) return <Spinner />;
+
+  if (error) return null;
   return (
     <List>
       {data.map((genre) => (
         <ListItem key={genre.id} paddingY={1}>
           <HStack>
-            <Image boxSize="32px" borderRadius = {8} src={getCroppedUrl(genre.image_background)}/>
-            <Text fontSize='lg'>{genre.name} </Text>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedUrl(genre.image_background)}
+            />
+            <Text fontSize="lg">{genre.name} </Text>
           </HStack>
         </ListItem>
       ))}
